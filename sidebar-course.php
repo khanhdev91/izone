79170c06327464ca6d529dfc3cae9af174a3aa93
khanh nguyen <khanhnd@yopaz.vn>
2021-04-04 00:12:23 +0700

    fix bugs

--- sidebar-course.php
+++ sidebar-course.php
@@ -55,26 +55,7 @@ $color = $temp[rand(0,3)];
             <h5 class="h3 mb-0 fs-14 fs-md-17 text-center">ĐĂNG KÝ GIỮ CHỖ</h5>
         </div>
         <div class="card-body p-3">
-            <form>
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-sm" id="" placeholder="Họ và tên">
-                </div>
-                <div class="form-group">
-                    <input type="email" class="form-control form-control-sm" id="" placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <input type="tel" class="form-control form-control-sm" id="" placeholder="Số điện thoại">
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-sm" id="" placeholder="Trường/Địa chỉ công tác">
-                </div>
-                <div class="form-group">
-                    <input type="date" class="form-control form-control-sm use-placeholder" id="" placeholder="Ngày sinh">
-                </div>
-                <div class="form-group mb-0">
-                    <button type="button" class="btn btn-info btn-block">ĐĂNG KÝ NGAY</button>
-                </div>
-            </form>
+            <?php echo do_shortcode('[sc name="register-form" ]'); ?>
         </div>
     </div>
 </div>
