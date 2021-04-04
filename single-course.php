2fade187252e0ecb01870795a5fb8dc2a2933b0c
khanh nguyen <khanhnd@yopaz.vn>
2021-04-04 17:25:12 +0700

    remove blocks

--- single-course.php
+++ single-course.php
@@ -13,29 +13,6 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-8">
-                    <div class="card sidebar-sticky mb-2" id="market-statistics-nav">
-                        <div class="card-body p-0">
-                            <nav class="navbar">
-                                <ul class="nav nav-pills">
-                                    <li class="">
-                                        <a class="active" href="#market-statistics-1">Thông tin chung</a>
-                                    </li>
-                                    <li class="">
-                                        <a class="" href="#market-statistics-2">Cấu trúc</a>
-                                    </li>
-                                    <li class="">
-                                        <a class="" href="#market-statistics-3">Phương pháp</a>
-                                    </li>
-                                    <li class="">
-                                        <a class="" href="#market-statistics-4">Giảng viên</a>
-                                    </li>
-                                    <li class="">
-                                        <a class="" href="#market-statistics-5">Phản hồi</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
                     <?php
                     while (have_posts()) :
                         the_post();
